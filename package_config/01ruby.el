508ecee4086d1908262d8d9cf8ec3e40a62bdd76
Tyler Morgan <tylermorgan86@gmail.com>
2014-02-19 11:48:34 -0800

    use ruby mode for Gemfile and rake files

--- package_config/01ruby.el
+++ package_config/01ruby.el
@@ -1,3 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
