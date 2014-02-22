81607ed3bdb81e7c576624819fee072e14ebfb36
Tyler Morgan <tylermorgan86@gmail.com>
2014-02-22 13:22:25 -0800

    adding vagrant and berksfiles to ruby mode

--- package_config/01ruby.el
+++ package_config/01ruby.el
@@ -1,5 +1,7 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
